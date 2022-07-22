82fd5d7c5b828e23bd8e26eb5a8995ccc2781450
TrexK <tejaskalpathi@gmail.com>
2022-07-22 00:05:27 -0500

    idk anymore

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import chaselogo from './chaselogo-removebg-preview.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="header">
+      <h1>JP MORGAN CHASE & CO.</h1>
+      <img id = "logo" src={chaselogo} alt="JPMorgan Chase Logo"/>
+
+    <section>
+      
+    </section>
     </div>
+
+   
   );
 }
 
