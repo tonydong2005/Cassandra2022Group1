3c19263f5ba776a9c6de2636fbadfe09e6bb9f5c
Stupid13579 <71733424+Stupid13579@users.noreply.github.com>
2022-07-24 11:56:39 -0500

    Delete ReactReduxSpringBoot directory

--- ReactReduxSpringBoot/src/store/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-import books from '../reducers/books';
-import keyspaces from '../reducers/keyspaces'
-import reducers from '../reducers/index'
-import thunk from 'redux-thunk';
-
-export default () => {
-    return createStore(reducers, applyMiddleware(thunk));
-};
