d9f6312cbd03d017d6f36b38b9e46cc0923d004c
Stupid13579 <tony.googoog@gmail.com>
2022-08-03 15:52:50 -0500

    more cleaning

--- expiremental/src/components/KeyspaceList.js
+++ expiremental/src/components/KeyspaceList.js
@@ -15,23 +15,17 @@
 
 import React, { useState, useEffect } from 'react';
 import { blue } from '@mui/material/colors';
-import { grey } from '@mui/material/colors';
 import TableList from './TableList';
 import axios from 'axios';
-import ListSubheader from '@mui/material/ListSubheader';
 import List from '@mui/material/List';
-import { Grid, Paper, Avatar, TextField, Box, Button, Typography, Link, Container } from '@mui/material';
+import { Typography } from '@mui/material';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Collapse from '@mui/material/Collapse';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import DraftsIcon from '@mui/icons-material/Drafts';
-import SendIcon from '@mui/icons-material/Send';
 import KeyIcon from '@mui/icons-material/Key';
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
-import StarBorder from '@mui/icons-material/StarBorder';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
